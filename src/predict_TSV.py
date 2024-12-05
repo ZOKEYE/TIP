1625cee152af1cf621a30d85deec6c6bacca57cb
ZOKEYE <78250953+ZOKEYE@users.noreply.github.com>
2024-12-05 20:20:45 +0800

    Update and rename predict_RBV.py to predict_TSV.py

--- src/predict_TSV.py
+++ src/predict_TSV.py
@@ -6,7 +6,7 @@ system = sys.platform
 import psutil
 from tqdm import tqdm
 
-from lib.divideData import divideRBVPredictData
+from lib.divideData import divideTSVPredictData
 from lib.operation import *
 from lib.log import *
 
@@ -26,10 +26,10 @@ def transX(x):
 
 
 def predictTask(args, process_num, slide_gene_name, slide_sequence, 
-                PID_fold, log_fold, RBV_fold, monitor_fold, monitor_len=100):
+                PID_fold, log_fold, TSV_fold, monitor_fold, monitor_len=100):
     # get parameters
-    rbv_model_fold = args.rbv_model_fold
-    rbv_model_num = args.rbv_model_num
+    tsv_model_fold = args.tsv_model_fold
+    tsv_model_num = args.tsv_model_num
 
     # Get the PID, bind it to the specified core, log it for process killing
     pid = os.getpid()
@@ -43,9 +43,9 @@ def predictTask(args, process_num, slide_gene_name, slide_sequence,
     txtSave(PID_file, [str(pid)])
 
     # create log file
-    log_file = '{}/predict_rbv_log_{}.log'.format(log_fold, process_num)
+    log_file = '{}/predict_tsv_log_{}.log'.format(log_fold, process_num)
     checkLog(log_file)
-    logger = createLogging(log_file, 'Predict RBV data')
+    logger = createLogging(log_file, 'Predict TSV data')
     logger.info('-----------------------Process {}(PID-{}) Start--------------------------'.format(process_num, pid))
 
     monitor_file = '{}/{}_0.txt'.format(monitor_fold, process_num)
@@ -56,7 +56,7 @@ def predictTask(args, process_num, slide_gene_name, slide_sequence,
     pred_len = len(slide_sequence)
     slide_data = transX(slide_sequence).reshape(pred_len, ntNum, 4, 1)
     predict = []
-    for model_idx in range(rbv_model_num):
+    for model_idx in range(tsv_model_num):
         # record
         if model_idx % monitor_len == 0:
             monitor_file = '{}/{}_{}.txt'.format(monitor_fold, process_num, model_idx // monitor_len)
@@ -67,7 +67,7 @@ def predictTask(args, process_num, slide_gene_name, slide_sequence,
             logger.info('Now using model {} to predict'.format(model_idx))
         
         # load model
-        model_name = '{}/{}_best_model.h5'.format(rbv_model_fold, model_idx)
+        model_name = '{}/{}_best_model.h5'.format(tsv_model_fold, model_idx)
         if not os.path.exists(model_name):
             print('Warning: The model named \'{}\' does not exist!'.format(model_name))
             continue
@@ -78,35 +78,35 @@ def predictTask(args, process_num, slide_gene_name, slide_sequence,
     predict = np.percentile(np.array(predict), 50, axis=0)
 
     # save data
-    RBV_file = '{}/pred_RBV_{}.csv'.format(RBV_fold, process_num)
+    TSV_file = '{}/pred_TSV_{}.csv'.format(TSV_fold, process_num)
     save_data = []
     for idx in range(pred_len):
         save_data.append([slide_gene_name[idx], predict[idx]])
-    csvSave(RBV_file, save_data)
+    csvSave(TSV_file, save_data)
     logger.info('-----------------------Finish--------------------------')
 
 
-def getRBV(rbv_output_fold, process_sum, origin_gene, origin_seq, origin_activity):
+def getTSV(tsv_output_fold, process_sum, origin_gene, origin_seq, origin_activity):
     # organize the results
-    RBV_fold = '{}/pred_rbv'.format(rbv_output_fold)
-    predict_rbv = []
+    TSV_fold = '{}/pred_tsv'.format(tsv_output_fold)
+    predict_tsv = []
     for p_num in range(process_sum):
-        RBV_file = '{}/pred_RBV_{}.csv'.format(RBV_fold, p_num)
-        predict = csvRead(RBV_file)
-        predict_rbv.extend(list(predict))
+        TSV_file = '{}/pred_TSV_{}.csv'.format(TSV_fold, p_num)
+        predict = csvRead(TSV_file)
+        predict_tsv.extend(list(predict))
 
     # transform predicted slice to original form
-    save_data, pred = [['Gene Name', 'UTR Sequence', 'Actual Activity', 'RBV Prediction']], []
+    save_data, pred = [['Gene Name', 'UTR Sequence', 'Actual Activity', 'TSV Prediction']], []
     i_origin = 0
     origin = origin_gene[i_origin]
     sequence = origin_seq[i_origin]
     activity = origin_activity[i_origin]
     last_gene = ''
-    for i in range(len(predict_rbv)):
-        gene = str.split(predict_rbv[i][0], '_')[0]
-        rbv = np.exp(float(predict_rbv[i][1]))
+    for i in range(len(predict_tsv)):
+        gene = str.split(predict_tsv[i][0], '_')[0]
+        tsv = np.exp(float(predict_tsv[i][1]))
         if gene == last_gene:
-            pred.append(rbv)
+            pred.append(tsv)
         else:
             if last_gene != '':
                 save_data.append(pred)
@@ -115,7 +115,7 @@ def getRBV(rbv_output_fold, process_sum, origin_gene, origin_seq, origin_activit
                 sequence = origin_seq[i_origin]
                 activity = origin_activity[i_origin]
             last_gene = gene
-            pred = [gene, sequence, activity, rbv]
+            pred = [gene, sequence, activity, tsv]
             while origin != last_gene:
                 save_data.append([origin, sequence, ''])
                 i_origin += 1
@@ -125,36 +125,36 @@ def getRBV(rbv_output_fold, process_sum, origin_gene, origin_seq, origin_activit
     return save_data
 
 
-def predictRBV(args):
+def predictTSV(args):
     # get parameters
-    rbv_model_num = args.rbv_model_num  # model nums
+    tsv_model_num = args.tsv_model_num  # model nums
     source_file = args.input_file  # input file
     process_sum = args.process_sum  # multiprocess
-    rbv_output_fold = args.rbv_output_fold  # output fold
+    tsv_output_fold = args.tsv_output_fold  # output fold
 
     # get divided data
-    origin_gene, origin_seq, divided_gene, divided_seq, Activity = divideRBVPredictData(args)
+    origin_gene, origin_seq, divided_gene, divided_seq, Activity = divideTSVPredictData(args)
 
     # create fold for recording PID
-    PID_fold = '{}/PID'.format(rbv_output_fold)
+    PID_fold = '{}/PID'.format(tsv_output_fold)
     if os.path.exists(PID_fold):
         removeFolds(PID_fold)
     os.mkdir(PID_fold)
 
     # create fold for recording log
-    log_fold = '{}/log'.format(rbv_output_fold)
+    log_fold = '{}/log'.format(tsv_output_fold)
     if os.path.exists(log_fold):
         removeFolds(log_fold)
     os.mkdir(log_fold)
     
     # create fold for predictions file
-    RBV_fold = '{}/pred_rbv'.format(rbv_output_fold)
-    if os.path.exists(RBV_fold):
-        removeFolds(RBV_fold)
-    os.mkdir(RBV_fold)
+    TSV_fold = '{}/pred_tsv'.format(tsv_output_fold)
+    if os.path.exists(TSV_fold):
+        removeFolds(TSV_fold)
+    os.mkdir(TSV_fold)
 
     # create fold for monitor
-    monitor_fold = '{}/monitor'.format(rbv_output_fold)
+    monitor_fold = '{}/monitor'.format(tsv_output_fold)
     if os.path.exists(monitor_fold):
         removeFolds(monitor_fold)
     os.mkdir(monitor_fold)
@@ -173,7 +173,7 @@ def predictRBV(args):
 
         # submit tasks
         p = Process(target=predictTask, args=(args, p_num, slide_gene_name, slide_sequence, 
-                                              PID_fold, log_fold, RBV_fold, monitor_fold, ))
+                                              PID_fold, log_fold, TSV_fold, monitor_fold, ))
         p.start()
         process_list.append(p)
 
@@ -181,17 +181,17 @@ def predictRBV(args):
         p.join()
     
     # organize the results
-    save_data = getRBV(rbv_output_fold, process_sum, origin_gene, origin_seq, Activity)
+    save_data = getTSV(tsv_output_fold, process_sum, origin_gene, origin_seq, Activity)
     return save_data
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='You could add these parameter.')
-    parser.add_argument('-i', '--input_file', type=str, default='./examples/input/sample_RBV_predict.xlsx', help='input file')
-    parser.add_argument('-rmn', '--rbv_model_num', type=int, default=2000, help='RBV model num')
-    parser.add_argument('-rmf', '--rbv_model_fold', type=str, default='./saved_models', help='rbv model fold')
-    parser.add_argument('-ro', '--rbv_output_fold', type=str, default='./examples/output', help='rbv output fold')
-    parser.add_argument('-rsf', '--rbv_save_file', type=str, default='', help='saved rbv file name')
+    parser.add_argument('-i', '--input_file', type=str, default='./examples/input/sample_TSV_predict.xlsx', help='input file')
+    parser.add_argument('-rmn', '--tsv_model_num', type=int, default=2000, help='TSV model num')
+    parser.add_argument('-rmf', '--tsv_model_fold', type=str, default='./saved_models', help='TSV model fold')
+    parser.add_argument('-ro', '--tsv_output_fold', type=str, default='./examples/output', help='TSV output fold')
+    parser.add_argument('-rsf', '--tsv_save_file', type=str, default='', help='saved TSV file name')
     parser.add_argument('-p', '--process_sum', type=int, default=cpu_count() // 2 - 1, help='process sum')
     parser.add_argument('-ss', '--server_sum', type=int, default=1, help='server sum')
     parser.add_argument('-sn', '--server_num', type=int, default=0, help='server num')
@@ -204,16 +204,16 @@ if __name__ == "__main__":
         print('ERROR: Please ensure the input file path exists!')
         exit()
     
-    save_data = predictRBV(args)
+    save_data = predictTSV(args)
     
-    save_rbv_file = args.rbv_save_file
-    if args.rbv_save_file == '':
+    save_tsv_file = args.tsv_save_file
+    if args.tsv_save_file == '':
         # remove old saved predicted file
         origin_file_name = str.split(args.input_file, '/')[-1]
         origin_file_name = str.split(origin_file_name, '.')[0]
-        save_rbv_file = '{}/pred_rbv_{}.csv'.format(args.rbv_output_fold, origin_file_name)
-    if os.path.exists(save_rbv_file):
-        os.remove(save_rbv_file)
+        save_tsv_file = '{}/pred_tsv_{}.csv'.format(args.tsv_output_fold, origin_file_name)
+    if os.path.exists(save_tsv_file):
+        os.remove(save_tsv_file)
 
     # save
-    csvSave(save_rbv_file, save_data)
+    csvSave(save_tsv_file, save_data)
